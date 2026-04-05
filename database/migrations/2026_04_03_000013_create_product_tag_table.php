81ecb49d7ab7ef205cedc8de040228677790a56f
jcparucha <jcparucha@gmail.com>
2026-04-05 16:31:20 +0800

    changed table name

--- database/migrations/2026_04_03_000013_create_product_tag_table.php
+++ database/migrations/2026_04_03_000013_create_product_tag_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('product_tags', function (Blueprint $table) {
+        Schema::create('product_tag', function (Blueprint $table) {
             $table->charset("utf8mb4");
             $table->collation("utf8mb4_unicode_ci");
 
