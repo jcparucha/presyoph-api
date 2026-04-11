af2adb6d0d250f32867ef98af59421d5bf74879e
jcparucha <jcparucha@gmail.com>
2026-04-11 23:12:33 +0800

    ## Others - installed Laravel Telescope for local development insights

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -11,7 +11,15 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
+        if (
+            $this->app->environment('local') &&
+            class_exists(\Laravel\Telescope\TelescopeServiceProvider::class)
+        ) {
+            $this->app->register(
+                \Laravel\Telescope\TelescopeServiceProvider::class,
+            );
+            $this->app->register(TelescopeServiceProvider::class);
+        }
     }
 
     /**
